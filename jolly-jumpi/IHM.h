45b7255950dbb6fb3783cfca43d472c0677e1276
tvaira <tvaira@free.fr>
2023-03-26 17:51:01 +0200

    Exemple multi-pages

--- jolly-jumpi/IHM.h
+++ jolly-jumpi/IHM.h
@@ -1,16 +1,45 @@
 #ifndef IHM_H
 #define IHM_H
 
+/**
+ * @file IHM.h
+ *
+ * @brief Déclaration de la classe IHM
+ * @author Axel LAPIERRE
+ * @version 0.1
+ */
+
 #include <QtWidgets>
+
+/**
+ * @def RASPBERRY_PI
+ * @brief Pour le mode plein écran sur la RPI
+ */
+//#define RASPBERRY_PI
 
 namespace Ui
 {
 class IHM;
 }
 
+/**
+ * @class IHM
+ * @brief Affiche les pages d'écran pour l'application
+ */
 class IHM : public QWidget
 {
     Q_OBJECT
+    /**
+     * @enum Page
+     * @brief Les différentes pages de la GUI
+     */
+    enum Page
+    {
+        Connexion,
+        Course,
+        NbPages
+    };
+
   public:
     IHM(QWidget* parent = nullptr);
     ~IHM();
@@ -25,6 +54,11 @@ class IHM : public QWidget
     void positionnerWidgets();
     void connecterSignauxSlots();
     void initialiserFenetre();
+
+  public slots:
+    void afficherPage(IHM::Page page);
+    void afficherPageConnexion();
+    void afficherPageCourse();
 };
 
 #endif // IHM_H
