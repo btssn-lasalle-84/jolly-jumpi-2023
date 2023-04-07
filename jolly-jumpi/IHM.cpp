8d39705fa5a8179e289ba47b489eebd075ce771f
alapierre-ops <98749326+alapierre-ops@users.noreply.github.com>
2023-04-07 11:14:06 +0200

    Optimisation du code

--- jolly-jumpi/IHM.cpp
+++ jolly-jumpi/IHM.cpp
@@ -80,29 +80,24 @@ void IHM::instancierWidgets()
 {
     ui->setupUi(this);
 
-    imageAvatarJoueur1 = new QPixmap("../Images/cheval1.png");
-    avatarJoueur1 = new QLabel(this);
+    imageAvatarsJoueurs.push_back(new QPixmap("../Images/cheval1.png"));
+    avatarsJoueurs.push_back(new QLabel(this));
 
-    imageAvatarJoueur2 = new QPixmap("../Images/cheval3.png");
-    avatarJoueur2 = new QLabel(this);
+    imageAvatarsJoueurs.push_back(new QPixmap("../Images/cheval3.png"));
+    avatarsJoueurs.push_back(new QLabel(this));
 
-    imageAvatarJoueur3 = new QPixmap("../Images/cheval2.png");
-    avatarJoueur3 = new QLabel(this);
+    imageAvatarsJoueurs.push_back(new QPixmap("../Images/cheval2.png"));
+    avatarsJoueurs.push_back(new QLabel(this));
 }
 
 void IHM::initialiserWidgets()
 {
-    *imageAvatarJoueur1 = imageAvatarJoueur1->scaled(QSize(200, 100));
-    avatarJoueur1->setPixmap(*imageAvatarJoueur1);
-    ui->pages->widget(IHM::Page::Course)->layout()->addWidget(avatarJoueur1);
-
-    *imageAvatarJoueur2 = imageAvatarJoueur2->scaled(QSize(200, 100));
-    avatarJoueur2->setPixmap(*imageAvatarJoueur2);
-    ui->pages->widget(IHM::Page::Course)->layout()->addWidget(avatarJoueur2);
-
-    *imageAvatarJoueur3 = imageAvatarJoueur3->scaled(QSize(200, 100));
-    avatarJoueur3->setPixmap(*imageAvatarJoueur3);
-    ui->pages->widget(IHM::Page::Course)->layout()->addWidget(avatarJoueur3);
+    for (int i = 0; i < imageAvatarsJoueurs.size(); i++)
+    {
+        *imageAvatarsJoueurs[i] = imageAvatarsJoueurs[i]->scaled(QSize(200, 100));
+        avatarsJoueurs[i]->setPixmap(*imageAvatarsJoueurs[i]);
+        ui->pages->widget(IHM::Page::Course)->layout()->addWidget(avatarsJoueurs[i]);
+    }
 }
 
 void IHM::positionnerWidgets()
